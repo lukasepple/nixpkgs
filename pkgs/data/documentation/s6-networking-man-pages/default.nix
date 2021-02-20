de056d6a095c80f468a28eef6e8cea09b73a4171
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-02-20 15:16:56 +0100

    s6-networking-man-pages: 2.4.0.0.2 -> 2.4.1.0.1

--- pkgs/data/documentation/s6-networking-man-pages/default.nix
+++ pkgs/data/documentation/s6-networking-man-pages/default.nix
@@ -2,8 +2,8 @@
 
 buildManPages {
   pname = "s6-networking-man-pages";
-  version = "2.4.0.0.2";
-  sha256 = "0gg2x4ixjr61iiayypiy3n4qf01c72gl9r9dam9dkik0r6x56mn7";
+  version = "2.4.1.0.1";
+  sha256 = "0j2jr90mdcbfmzfjzk0j95j8najy1mjg16xd0cm2d6g9gpz5zk2x";
   description = "Port of the documentation for the s6-networking suite to mdoc";
   sections = [ 1 7 ];
   maintainers = [ lib.maintainers.sternenseemann ];
