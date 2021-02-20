c3b4e17c8b3e7d95a8236d66a67f33754cef1916
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-02-20 15:16:40 +0100

    s6-man-pages: 2.10.0.1.2 -> 2.10.0.2.1

--- pkgs/data/documentation/s6-man-pages/default.nix
+++ pkgs/data/documentation/s6-man-pages/default.nix
@@ -2,8 +2,8 @@
 
 buildManPages {
   pname = "s6-man-pages";
-  version = "2.10.0.1.2";
-  sha256 = "1r2gyjwma4v6v5v5by4ivkk9jagn6006l5dcpsli4xijg0v7mq1v";
+  version = "2.10.0.2.1";
+  sha256 = "1i40ylhr5lbmv4ggv4w37r96qqnnqqljqqqc3c3zgvpflwnzy4h8";
   description = "Port of the documentation for the s6 supervision suite to mdoc";
   sections = [ 1 7 ];
   maintainers = [ lib.maintainers.sternenseemann ];
