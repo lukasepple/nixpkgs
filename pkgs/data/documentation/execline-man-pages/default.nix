3c05c6b267c5a08508760c2e5e03d64d8e08e363
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-04-27 10:18:56 +0200

    execline-man-pages: 2.8.0.0.1 -> 2.8.0.1.1

--- pkgs/data/documentation/execline-man-pages/default.nix
+++ pkgs/data/documentation/execline-man-pages/default.nix
@@ -2,8 +2,8 @@
 
 buildManPages {
   pname = "execline-man-pages";
-  version = "2.8.0.0.1";
-  sha256 = "0nyhkjbxyffcfyr7h7wgyj2znm4g1cz9010a5cjk097lj53hy1f4";
+  version = "2.8.0.1.1";
+  sha256 = "0xv9v39na1qnd8cm4v7xb8wa4ap3djq20iws0lrqz7vn1w40i8b4";
   description = "Port of the documentation for the execline suite to mdoc";
   sections = [ 1 7 ];
   maintainers = [ lib.maintainers.sternenseemann ];
